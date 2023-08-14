39ec607696a2613877f9b830561cb5b0da2422d9
Komal Ranvir <komalranvir07@gmail.com>
2023-08-14 14:07:26 +0530

    project file add

---
+++ src/main/java/komal/CafeManagementApp/CafeManagementAppApplication.java
@@ -0,0 +1,13 @@
+package komal.CafeManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/komal/CafeManagementApp/CafeManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package komal.CafeManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
